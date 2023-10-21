356cdcf99036289f54498ecc0c77e6a8ac2acb7d
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-10-21 16:58:32 +0700

    Finishing Update User

--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -6,6 +6,9 @@ export default async function handler(req, res){
         case "GET" :
             return await getUser(req, res)
 
+        case "PUT" :
+            return await updateUser(req, res)
+
         case "DELETE" :
             return await deleteUser(req, res)
     }
@@ -29,6 +32,33 @@ const getUser = async (req, res) => {
         return res.status(500).json(error.message);
     }
 }
+
+const updateUser = async (req, res) => {
+    try{
+        // New Username and email
+        const {email, username} = req.body;
+
+        // ID
+        const {userId} = req.query;
+
+        const result = await prisma.user.update({
+            where: {
+                id: parseInt(userId)
+            },
+
+            data: {
+                username: username,
+                email: email
+            }
+        })
+
+        return res.status(200).json({username, email, userId});
+
+    }catch(error){
+        return res.status(500).json(error.message);
+    }
+}
+
 const deleteUser = async (req, res) => {
     try{
         const {userId} = req.query;
