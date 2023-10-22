587cd182923120f7a3bb1c85caf5977ceb6bafe6
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-10-22 11:10:33 +0700

    Fixing Alert

--- components/Alert.js
+++ components/Alert.js
@@ -1,9 +1,15 @@
-const Alert = () => {
+const Alert = ({text, style, setAlertMessage}) => {
+
+    const handleAlert = (e) => {
+        e.preventDefault();
+        setAlertMessage("")
+    }
+
     return (
         <>
-            <div className="alert w-50 align-self-center alert-success alert-dismissible fade show" role="alert">
-                Data Added successfully
-                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <div className="alert w-50 align-self-center alert-success alert-dismissible fade show" style={{display: style}} role="alert">
+                {text}
+                <button type="button" className="close" onClick={(e) => handleAlert(e)}>
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
