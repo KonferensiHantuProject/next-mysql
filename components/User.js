74f77c6ce28816eda3bf97902ae2e1fd9414eeaf
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-10-22 11:02:00 +0700

    Finishing Delete

--- components/User.js
+++ components/User.js
@@ -1,4 +1,4 @@
-const User = ({user}) => {
+const User = ({user, handleDelete}) => {
     return (
         <>
             <tr>
@@ -13,7 +13,7 @@ const User = ({user}) => {
                 <td>{user.email}</td>
                 <td>
                     <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                    <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                    <a href="#deleteEmployeeModal" onClick={() => handleDelete(user.id)} className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
                 </td>
             </tr>
         </>
