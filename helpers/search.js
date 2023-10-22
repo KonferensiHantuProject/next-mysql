5cd9b7454ce1fa40130413c1233773ff15fe1a7e
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-10-22 08:19:41 +0700

    Adding Search Feature

---
+++ helpers/search.js
@@ -0,0 +1,7 @@
+export const Search = (data, searchQuery) => {
+    const searchedData = data.filter(element => {
+        return element.username.includes(searchQuery);
+    });
+
+    return searchedData;
+}
